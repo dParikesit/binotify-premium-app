ddbe2ad9c6158f76e7eeb6c2e4f0e99cc590a7a5
pandora-1 <michaelkahari.mk@gmail.com>
2022-11-23 02:27:00 +0700

    add color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    colors: {
+      'green': {
+          '100': '#1DB954',
+          '200': '#00A573',
+          '300': '#008F84',
+          '400': '#007784',
+          '500': '#005F74',
+          '600': '#2F4858'
+      },
+      'white': '#FFFFFF'
+  },
+    extend: {},
+  },
+  plugins: [],
+}
